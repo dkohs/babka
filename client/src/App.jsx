e72877c5419b988ed7ef84144be1b83e3c51b60b
Olivia Suh <olivia@Olivias-Air.lan>
2025-04-05 09:11:07 -0700

    Olivia completed server and cklient set up

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import {Box} from '@mui/material'
+import {Navbar} from './lib/NavBar'
+
+export const App = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require("express");
+const { connection } = require("./db/connection");
+const {userRoutes} = require("./routes/userRoutes");
+const {authRoutes} = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const runServer = async () => {
+  try {
+    await connection;
+
+    app.use("/api/users", userRoutes);
+    app.use("/api/auth", authRoutes);
+    app.use("/api/activities", activityRoutes)
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1);
+  }
+}
+
+runServer();
